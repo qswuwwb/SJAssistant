240e4105f652460db5c279244e3d339826864e69
qswuwwb <YinSJ.init@gmail.com>
2016-10-25 23:16:48 +0800

    add gitignore

--- Attendance/urls.py
+++ Attendance/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
 from .views import AttendanceList, attendanceCreate
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
@@ -7,3 +8,5 @@ urlpatterns = [
     # url(r'^submit/', login_required(AttendanceCreate.as_view(template_name='Attendance/attendance_form.html')))
     url(r'^submit/', attendanceCreate)
 ]
+
+urlpatterns += staticfiles_urlpatterns()
